d4c5d38ff8fd69ed03d068c824ff550507822b49
Matas_Pieczulis <51287415+Matajza123@users.noreply.github.com>
2020-01-20 21:31:05 +0100

    Update __init__.py

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -7,7 +7,7 @@ from flask_caching import Cache
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = 'Super Secret Key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
